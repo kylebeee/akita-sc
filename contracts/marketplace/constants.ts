2ef05510f3599aa79460c86dab225f9f3094b323
Chris Kim (Hyunggun) <chris.kim@algorand.foundation>
2025-04-02 05:15:20 +0200

    feat: wip-marketplace migration

--- contracts/marketplace/constants.ts
+++ contracts/marketplace/constants.ts
@@ -1,3 +1,4 @@
 export const AKITA_LISTING_PRICE_KEY = 'price'
 export const AKITA_LISTING_PAYMENT_ASSET_KEY = 'payment_asset'
 export const AKITA_LISTING_SELLER_KEY = 'seller'
+export const AkitaMarketplaceGlobalStateKeysChildContractVersion = 'child_contract_version'
