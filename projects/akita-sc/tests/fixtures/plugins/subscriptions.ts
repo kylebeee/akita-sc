e60309e5400287eb76dda14783453bb8132298e1
krby.algo <krby@akita.community>
2025-11-19 19:32:35 -0800

    feat: dao self updating & test deployers

---
+++ projects/akita-sc/tests/fixtures/plugins/subscriptions.ts
@@ -0,0 +1,28 @@
+
+
+import { PayPluginFactory } from '../../../smart_contracts/artifacts/arc58/plugins/pay/PayPluginClient';
+import { FixtureAndAccount } from '../../types';
+import { PayPluginSDK } from 'akita-sdk'
+
+type DeployParams = FixtureAndAccount
+
+export const deploySubscriptionsPlugin = async ({ fixture, sender, signer }: DeployParams): Promise<PayPluginSDK> => {
+  const { algorand } = fixture.context;
+
+  const factory = algorand.client.getTypedAppFactory(
+    PayPluginFactory,
+    {
+      defaultSender: sender,
+      defaultSigner: signer,
+    }
+  )
+
+  const { appClient: client } = await factory.deploy({
+    onUpdate: 'append',
+    onSchemaBreak: 'append',
+  })
+
+  console.log('PayPlugin deployed with appId:', client.appId);
+
+  return new PayPluginSDK({ algorand, factoryParams: { appId: client.appId } });
+};
